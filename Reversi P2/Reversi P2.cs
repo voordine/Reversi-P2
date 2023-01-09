7977fa34fee209d6b99937e3f71133a94e4de774
voordine <nadinekraakman@yahoo.com>
2023-01-09 18:04:40 +0100

    hi

--- Reversi P2/Reversi P2.cs
+++ Reversi P2/Reversi P2.cs
@@ -55,6 +55,10 @@ size10.Location = new Point(675, 445);
 size10.Size = new Size(100, 50);
 size10.Text = "10 x 10";
 
+Label zet = new Label();
+zet.Location = new Point(360, 40);
+zet.Size = new Size(200, 20);
+
 scherm.Controls.Add(board);
 scherm.Controls.Add(help);
 scherm.Controls.Add(rules);
@@ -64,47 +68,47 @@ scherm.Controls.Add(size4);
 scherm.Controls.Add(size6);
 scherm.Controls.Add(size8);
 scherm.Controls.Add(size10);
+scherm.Controls.Add(zet);
 
 //Players
 Label PlayerBl = new Label();
 PlayerBl.Location = new Point(45, 20);
 PlayerBl.Size = new Size(60, 60);
-scherm.Controls.Add(PlayerBl);
 
 Label PlayerWh = new Label();
 PlayerWh.Location = new Point(695, 20);
 PlayerWh.Size = new Size(60, 60);
-scherm.Controls.Add(PlayerWh);
+
+Label statsbl = new Label();
+statsbl.Location = new Point(130, 40);
+statsbl.Size = new Size(60, 60);
+//stenenzwart.Text = "ja dus";
+
+Label statswh = new Label();
+statswh.Location = new Point(620, 40);
+statswh.Size = new Size(60, 60);
+//stenenwit.Text = "werkt dit";
 
 void player1(object sender, PaintEventArgs pea)
-{ 
+{
     pea.Graphics.FillEllipse(Brushes.Black, 0, 0, 60, 60);
-} PlayerBl.Paint += player1;
+}
 
 void player2(object sender, PaintEventArgs pea)
 {
     pea.Graphics.FillEllipse(Brushes.White, 0, 0, 60, 60);
-} PlayerWh.Paint += player2;
+}
 
-Label zet = new Label();
-zet.Location = new Point(360, 40);
-zet.Size = new Size(200, 20);
-scherm.Controls.Add(zet);
+scherm.Controls.Add(PlayerBl);
+scherm.Controls.Add(PlayerWh);
+scherm.Controls.Add(statsbl);
+scherm.Controls.Add(statswh);
+PlayerBl.Paint += player1;
+PlayerWh.Paint += player2;
 
-Label stenenwit = new Label();
-stenenwit.Location = new Point(620, 40);
-stenenwit.Size = new Size(60, 60);
-stenenwit.Text = "werkt dit";
-scherm.Controls.Add(stenenwit);
-
-Label stenenzwart = new Label();
-stenenzwart.Location = new Point(130, 40);
-stenenzwart.Size = new Size(60, 60);
-stenenzwart.Text = "ja dus";
-scherm.Controls.Add(stenenzwart);
 #endregion
 
-int n = 10;
+int n = 6;
 int[,] velden = new int[n, n];
 
 InitializeBoard(); //board.Invalidate();
@@ -114,37 +118,43 @@ void click4(object o, EventArgs ea)
     n = 4;
     InitializeBoard();
     board.Invalidate();
-}size4.Click += click4;
+}
+size4.Click += click4;
 
 void click6(object o, EventArgs ea)
 {
     n = 6;
     InitializeBoard();
     board.Invalidate();
-}size6.Click += click6;
+}
+size6.Click += click6;
 
 void click8(object o, EventArgs ea)
 {
     n = 8;
     InitializeBoard();
     board.Invalidate();
-}size8.Click += click8;
+}
+size8.Click += click8;
 
 void click10(object o, EventArgs ea)
 {
     n = 10;
     InitializeBoard();
     board.Invalidate();
-}size10.Click += click10;
+}
+size10.Click += click10;
 
 void playagain(object o, EventArgs ea)
 {
     InitializeBoard();
     board.Invalidate();
 
-} newgame.Click += playagain;
+}
+newgame.Click += playagain;
 void InitializeBoard()
 {
+    velden = new int[n, n];
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -152,7 +162,6 @@ void InitializeBoard()
             velden[i, j] = 0;
         }
     }
-
     velden[n / 2 - 1, n / 2 - 1] = 1;
     velden[n / 2, n / 2] = 1;
     velden[n / 2 - 1, n / 2] = 2;
@@ -189,18 +198,145 @@ void DrawBoard(object o, PaintEventArgs pea)
 
 }
 
-int beurt = 1;
+int breedte = board.Width / n;
+int hoogte = board.Height / n;
+
+int turn = 1;
+
+//Methode die de stenen telt van beide spelers in een array. ook lege velden worden geteld
+void CountPieces()
+{
+    int BlackPieces = 0;
+    int WhitePieces = 0;
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+        {
+            if (velden[i, j] == 1)
+            {
+                BlackPieces++;
+            }
+            else if (velden[i, j] == 2)
+            {
+                WhitePieces++;
+            }
+        }
+    //converteren naar strings zodat hij als tekst op de labels kan
+    string BlackPiecesstr = BlackPieces.ToString(); 
+    string WhitePiecesstr = WhitePieces.ToString();
+    statsbl.Text = BlackPiecesstr;
+    statswh.Text = WhitePiecesstr;
+    
+} 
+
+/*void SwitchPlayer(bool moremoves = false)
+{
+    if (turn == 1)
+        turn = 2;
+    else if (turn == 2)
+        turn = 1;
+
+    //Hier de legaliteit herchecken, en veld hertekenen
+    bool geenzetten = true;
+    for (int x = 0; x < breedte; x++)
+    {
+        for (int y = 0; y < hoogte; y++)
+        {
+            //Als er nog geen legale zetten zijn, moet geenzetten gecheckt worden
+            if (geenzetten)
+                if (CheckLegal(x, y))
+                {
+                    geenzetten = false;
+                    velden[x, y].Legaal = true;
+                }
+            velden[x, y].Legaal = CheckLegal(x, y);
+        }
+    }
+}*/
+//Check de legaliteit van het vlak op x, y in elke richting
+//Zet is illegaal, behalve als er een legale richting gevonden wordt
+bool CheckLegal(int x, int y)
+{
+    if (velden[x, y] != 0)
+        return false;
+    for (int m = -1; m <= 1; m++)
+        for (int n = -1; n <= 1; n++)
+            if (!(n == 0 && m == 0))
+                if (outflank(x + m, y + n, m, n))
+                    return true;
+    return false;
+}
+
+//Vindt in elke richting een insluiter (stopt dus niet bij de eerste vinder) en speelt als gevonden
+void PlayReversi(int x, int y)
+{
+    //We gaan in elke richting een insluiter zoeken, en als die wordt gevonden,
+    //wordt speelstenen vanaf daar getriggert
+    for (int m = -1; m <= 1; m++)
+        for (int n = -1; n <= 1; n++)
+            if (!(n == 0 && m == 0))
+                outflank(x + m, y + n, m, n, true);
+}
+
+//Vind een insluitende steen met minstends één rode ertussen
+bool outflank(int x, int y, int newx, int newy, bool play = false, bool first = true)
+{
+    if (x < 0 || y < 0 || x >= breedte || y >= hoogte)
+    {
+        //Hij zoekt buiten het veld, dus niet op tijd gevonden
+        return false;
+    }
+    if (velden[x, y] == turn)
+    {
+        //Als de aanliggende steen meteen van dezelfde kleur is, is insluiter false
+        if (first == true)
+            return false;
+
+        //Er is een insluitende steen gevonden, als het een zet is moet er worden gespeeld
+        //Anders alleen return
+        if (play)
+            Moves(-newx + x, -newy + y, -newx, -newy);
+
+        return true;
+    }
+    else
+    {
+        //Check of deze plek niet leeg is
+        if (velden[x, y] == 0)
+            return false;
+
+        //Er ligt een steen van de tegenstander
+        //zet de plaats een richting verder
+        x = x + newx;
+        y = y + newy;
+
+        return outflank(x, y, newx, newy, play, false);
+    }
+}
+
+//Speel stenen terug tot de beurt wordt teruggevonden
+void Moves(int x, int y, int newx, int newy)
+{
+    //Blijf stenen van naar eigen kleur zetten tot eigen kleur wordt teruggevonden
+    //Speelstenen wordt aangeroepen door een legaliteitscheck, dat hoeft hier niet dubbel
+    if (velden[x, y] != turn)
+    {
+        //In toestand.set zit een automatische invalidate, dus wordt meteen getekend
+        velden[x, y] = turn;
+        Moves(x + newx, y + newy, newx, newy);
+    }
+}
 void zetsteen(object sender, MouseEventArgs mea)
 {
     int x = n * mea.X / board.Width;
     int y = n * mea.Y / board.Height;
-   
-        if (beurt == 1)
-            { velden[x, y] = 1; }
 
-        else if (beurt == 2)
-            { velden[x, y] = 2; }
-        
+    if (turn == 1)
+    { velden[x, y] = 1; }
+
+    else if (turn == 2)
+    { velden[x, y] = 2; }
+
     board.Invalidate();
 }
 board.MouseClick += zetsteen;
@@ -208,11 +344,12 @@ board.MouseClick += zetsteen;
 //methode om van een int voor de speler een string te maken
 string Beurtstring()
 {
-    if (beurt == 1)
+    if (turn == 1)
         return "Black's turn";
     else
         return "White's turn";
-}zet.Text = Beurtstring();
+}
+zet.Text = Beurtstring();
 
 void showrules(object o, EventArgs e)
 {
